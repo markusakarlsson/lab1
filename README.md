d20be788e378eb24b8421c1d5ed77971a720d21a
Markus Karlsson <markuskarlsson__@hotmail.com>
2019-11-04 23:27:21 +0100

    game finished

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lab1
+# Lab1
 En textbaserad interaktiv upplevelse.
+
+## Beskrivning av uppgiften
+I den här laborationen skall du skapa en textbaserad interaktiv upplevelse där användaren kan göra förändringar i en värld. Exempel på sådana förändringar är: gå till ett annat rum, plocka upp föremål, prata med entiteter etc. Uppgiften är fri och du väljer själv hur din värld ska se ut och hur användaren interagerar i världen. Låt kreativiteten flöda!
+
+## GitHub
+Länk till repot: https://github.com/markusakarlsson/lab1
+
+Länk till pages: https://markusakarlsson.github.io/lab1/
--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function start() {
 
     if (choice === "sovrummet") {
         loadSovrummet() 
-    } else if (choice === "vardagsrummet") {
+    } else if (choice === "vardagsrummet") 
         loadVardagsrummet()
-    } else {
-        // berätta för användaren att hen matade in ett felaktigt val
-    }
 }
 
+/**
+ * @param loadSovrummet - Information about the current room and make next choice.
+ */
 function loadSovrummet() {
     text.innerHTML = "Här fanns flera ställen där den kan ligga... Vill du titta i nattduksbordet eller garderoben?"
     button.onclick = room1
@@ -26,13 +26,13 @@ function room1() {
     
     if (choice === "nattduksbordet") {
         loadNattduksbordet()
-    } else if (choice === "garderoben") {
+    } else if (choice === "garderoben")
         loadGarderoben()
-    } else {
-        // berätta för användaren att hen matade in ett felkaktigt val
-    }
 }
 
+/**
+ * @param loadNattduksbordet - Information about the current place and make next choice.
+ */
 function loadNattduksbordet() {
     text.innerHTML = "Oj, här var det tomt.... Vill du titta i garderoben eller gå vidare till köket?"
     button.onclick = room2
@@ -44,13 +44,13 @@ function room2() {
     
     if (choice === "garderoben") {
         loadGarderoben()
-    } else if (choice === "köket") {
+    } else if (choice === "köket")
         loadKöket()
-    } else {
-        // berätta för användaren att hen matade in ett felkaktigt val
-    }
 }
 
+/**
+ * @param loadGarderoben - Information about the current room and make next choice.
+ */
 function loadGarderoben() {
     text.innerHTML = "Herregud vad kläder det var här, det är nog bäst om du letar vidare... Vill du fortsätta till köket eller vardagsrummet?"
     button.onclick = room3
@@ -63,13 +63,13 @@ function room3() {
     
     if (choice === "köket") {
         loadKöket()
-    } else if (choice === "vardagsrummet") {
+    } else if (choice === "vardagsrummet")
         loadVardagsrummet()
-    } else {
-        // berätta för användaren att hen matade in ett felkaktigt val
-    }
 }
 
+/**
+ * @param loadVardagsrummet - Information about the current room and make next choice.
+ */
 function loadVardagsrummet() {
     text.innerHTML = "Vilken jobbig tystnad det var här, spring snabbt vidare!! Köket eller sovrummet up next?"
     button.onclick = room4
@@ -81,13 +81,13 @@ function room4() {
     
     if (choice === "köket") {
         loadKöket()
-    } else if (choice === "sovrummet") {
+    } else if (choice === "sovrummet") 
         loadSovrummet()
-    } else {
-        // berätta för användaren att hen matade in ett felkaktigt val
-    }
 }
 
+/**
+ * @param loadKöket - Information about the current room and make next choice.
+ */
 function loadKöket() {
     text.innerHTML = "En fjärrkontroll är väl ingen kylvara direkt men med en törstig ägare kan allt hända... Vill du öppna kylen och titta?"
     button.onclick = room5
@@ -99,20 +99,23 @@ function room5() {
     
     if (choice === "ja") {
         loadFjärrkontroll()
-    } else if (choice === "nej") {
+    } else if (choice === "nej")
         loadGameOver()
-    } else {
-        // berätta för användaren att hen matade in ett felkaktigt val
-    }
 }
 
+/**
+ * @param loadFjärrkontroll - Game completed positively.
+ */
 function loadFjärrkontroll() {
     text.innerHTML = "Gött!! Du hittade den och min kväll är räddad!"
-    document.body.style.backgroundImage = "url('/remote.jpg')"
+    document.body.style.backgroundImage = "url('./remote.jpg')"
     document.getElementById("input").style.display = "none"
     document.getElementById("button").style.display = "none"
 }
 
+/**
+ * @param loadGameOver - Game completed negatively.
+ */
 function loadGameOver() {
     text.innerHTML = "Game Over, jag vill inte ha hjälp av någon som inte letar överallt..... Ut ur min lägenhet!!"
     document.getElementById("input").style.display = "none"
